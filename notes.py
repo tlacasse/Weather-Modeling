6df0e638c72791ada85f405aba638b21cd199fdf
Thomas Lacasse <thomas@kukt.us>
2019-12-22 01:50:33 -0500

    remove old files

--- notes.py
+++
@@ -1,8 +0,0 @@
-
-# add geojson (leaflet js)
-# convert shp file to geojson
-# take sequence without last
-#    then verterbi and update model using normal of window subset of transition matrix?
-#    with noise
-#    then determine that final value we want
-
--- test3.py
+++
@@ -1,8 +0,0 @@
-import requests
-from pprint import pprint
-
-uri = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets'
-headers = {'token': 'GXVZhqHZGNxUJyWNQNTRYlxBuNGWjZvr'}
-
-r = requests.get(uri, headers=headers)
-pprint(r.json())
